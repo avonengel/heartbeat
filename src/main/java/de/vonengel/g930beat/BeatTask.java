80248e489e580dc939acc865263cf96bf16ec014
Axel von Engel <a.vonengel@gmail.com>
2015-02-13 19:57:11 +0100

    Refactored Timer creation and disposal

---
+++ src/main/java/de/vonengel/g930beat/BeatTask.java
@@ -0,0 +1,62 @@
+package de.vonengel.g930beat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.TimerTask;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer.Info;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class BeatTask extends TimerTask {
+    private static final Logger LOG = LoggerFactory.getLogger(BeatTask.class);
+    private final Clip clip;
+    private AudioInputStream ais;
+
+    BeatTask(Info info, URL url) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        LOG.info("Starting heartbeat:");
+        LOG.info("Mixer: {}", info.getName());
+        this.clip = AudioSystem.getClip(info);
+        ais = initAudioInput(url);
+        clip.open(ais);
+    }
+
+    private AudioInputStream initAudioInput(URL url) throws IOException, UnsupportedAudioFileException {
+        assert url != null;
+        return AudioSystem.getAudioInputStream(url);
+    }
+
+    public void run() {
+        LOG.info("Sending heartbeat");
+        try {
+            clip.setFramePosition(0);
+            clip.start();
+        } catch (Exception e) {
+            LOG.error("Error playing clip", e);
+            closeClip();
+        }
+    }
+
+    @Override
+    public boolean cancel() {
+        closeClip();
+        return super.cancel();
+    }
+
+    private void closeClip() {
+        if (clip.isOpen()) {
+            clip.close();
+            try {
+                ais.close();
+            } catch (IOException e) {
+                LOG.error("Error closing AudioInputStream", e);
+            }
+        }
+    }
+}
--- src/main/java/de/vonengel/g930beat/Heartbeat.java
+++ src/main/java/de/vonengel/g930beat/Heartbeat.java
@@ -1,6 +1,5 @@
 package de.vonengel.g930beat;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,44 +9,34 @@ import java.util.NoSuchElementException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.Mixer.Info;
 import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Heartbeat {
     private static final Logger LOG = LoggerFactory.getLogger(Heartbeat.class);
-    private Map<String, Timer> runningBeats = new HashMap<>();
+    private Map<String, TimerTask> activeBeats = new HashMap<>();
     private HeartbeatPreferences preferences = new HeartbeatPreferences();
+    private Timer timer;
+
+    public Heartbeat() {
+        timer = new Timer("Heartbeat930", true);
+    }
 
     public void start(String mixerName) throws Exception {
-        AudioInputStream ais = loadAudioInput();
         Mixer.Info mixerInfo = pickMixer(mixerName);
-        final Clip clip = AudioSystem.getClip(mixerInfo);
-        clip.open(ais);
-        logParameters(mixerInfo);
-        TimerTask task = new TimerTask() {
-            public void run() {
-                Heartbeat.LOG.info("Sending heartbeat");
-                try {
-                    clip.setFramePosition(0);
-                    clip.start();
-                } catch (Exception e) {
-                    LOG.error("Error playing clip", e);
-                    clip.close();
-                }
-            }
-        };
-        Timer timer = new Timer("Heartbeat930-" + mixerName, true);
-        runningBeats.put(mixerName, timer);
+        TimerTask task = new BeatTask(mixerInfo, getFileUrl());
+        activeBeats.put(mixerName, task);
         timer.scheduleAtFixedRate(task, 0L, computePeriod());
+    }
+
+    private URL getFileUrl() {
+        return getClass().getClassLoader().getResource(preferences.getFile());
     }
 
     public Mixer.Info pickMixer(String desiredName) {
@@ -59,22 +48,8 @@ public class Heartbeat {
         throw new NoSuchElementException(desiredName);
     }
 
-    private void logParameters(Mixer.Info mixerInfo) {
-        LOG.info("Starting heartbeat:");
-        LOG.info("Mixer: {}", mixerInfo.getName());
-        LOG.info("File: {}", preferences.getFile());
-        LOG.info("Period: {}", Long.valueOf(preferences.getPeriod()));
-    }
-
     public void stop() {
-        runningBeats.forEach((name, timer) -> timer.cancel());
-    }
-
-    private AudioInputStream loadAudioInput() throws IOException, UnsupportedAudioFileException {
-        URL url = super.getClass().getClassLoader().getResource(preferences.getFile());
-        assert url != null;
-        AudioInputStream ais = AudioSystem.getAudioInputStream(url);
-        return ais;
+        activeBeats.forEach((name, beat) -> beat.cancel());
     }
 
     private long computePeriod() {
@@ -94,9 +69,9 @@ public class Heartbeat {
     }
 
     public void cancelMixer(String mixerName) {
-        Timer timer = runningBeats.get(mixerName);
-        if (timer != null) {
-            timer.cancel();
+        TimerTask beatTask = activeBeats.get(mixerName);
+        if (beatTask != null) {
+            beatTask.cancel();
         }
     }
 }
